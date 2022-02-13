c0cc23fdc68a673acb6862eee9b0c8fe78957893
OsmanRana <osmancmc@yahoo.com>
2022-02-13 12:23:54 +0600

    some API created

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { MongoClient } = require("mongodb");
 const app = express();
 require("dotenv").config();
 const cors = require("cors");
+const ObjectId = require("mongodb").ObjectId;
 const port = process.env.PORT || 5000;
 
 //middleware
@@ -21,13 +22,84 @@ async function run() {
 
     const database = client.db("toysWorld");
     const toysCollections = database.collection("toys");
+    const usersCollections = database.collection("users");
+
+    // Toys Collection
+    // send all toys
+    app.get("/toys", async (req, res) => {
+      const cursor = toysCollections.find({});
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    //send single toy by id
+    app.get("/toys/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await toysCollections.findOne(query);
+      res.send(result);
+    });
+
+    // send toys by age bracket
+    app.get("/shopByAge/:ageBracket", async (req, res) => {
+      const ageBracket = req.params.ageBracket;
+      const query = { ageBracket: ageBracket };
+      const cursor = toysCollections.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     //add new toys
-    app.post("/toys", async (req, res) => {
+    app.post("/addToys", async (req, res) => {
       const toy = req.body;
       const result = await toysCollections.insertOne(toy);
       res.send(result);
-      console.log(toy);
+    });
+
+    //update a toy
+    app.put("/updateToys/:id", async (req, res) => {
+      const id = req.params.id;
+      const update = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          name: update.name,
+          description: update.description,
+          ageBracket: update.ageBracket,
+          brand: update.brand,
+          price: update.price,
+          image: update.image,
+        },
+      };
+      const result = await toysCollections.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
+    //delete toy
+    app.delete("/toys/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await toysCollections.deleteOne(query);
+      res.send(result);
+    });
+
+    //Users Collection
+    //add user
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollections.insertOne(user);
+      res.send(result);
+    });
+
+    //send a user
+    app.get("/users", async (req, res) => {
+      const users = usersCollections.find({});
+      const result = await users.toArray();
+      res.send(result);
     });
   } finally {
     // await client.close()
